9be2f0af7080d3d3e8dcb529171c6a1d9f466f2f
Matthew Willard <MWillard85@gmail.com>
2019-06-06 17:21:42 -0600

    learning tabs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-//import Counter from './state/Counter';
-//import Messages from './Messages';
-//import TheDate from './state/TheDate';
-//import HelloWorld from './state-drills/HelloWorld'
-//import RouletteGun from './state-drills/RouletteGun'
-import Bomb from './state-drills/Bomb'
+import Tabs from './state/Tabs';
 
-class App extends Bomb {
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+
+class App extends Component {
   render() {
     return (
-      <div className='CountdownBomb'>
-      {this.renderDisplay()}
+      <div>
+        <Tabs tabs={tabsProp} />
     </div>
       
     )
